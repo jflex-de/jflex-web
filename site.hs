6ab6ccee05ef7aad4b390170f5915f9e49a886ed
Gerwin Klein <gerwin.klein@nicta.com.au>
2015-04-06 19:11:54 +1000

    automatically insert github issue links

--- site.hs
+++ site.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Monoid (mappend)
 import Hakyll
+import Text.Regex.TDFA ((=~))
 
 ----- JFlex hakyll site script -----
 
@@ -26,10 +26,14 @@ main = hakyll $ do
     match "pages/*.md" $ do
         route   $ setExtension "html" `composeRoutes` 
                   gsubRoute "pages/" (const "")
-        compile $ pandocCompiler
+        compile $ do
+          body <- getResourceBody
+          filtered <- return $ fmap issueFilter body
+          return (renderPandoc filtered)
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
+    -- raw html include, does not apply issue filter
     match "pages/*.html" $ do
         route   $ gsubRoute "pages/" (const "")
         compile $ getResourceBody
@@ -39,3 +43,23 @@ main = hakyll $ do
     match "templates/*" $ compile templateCompiler
 
 
+-----------------------
+
+issueFilter :: String -> String
+issueFilter markdown = substAll "#[0-9]{1,4}[^0-9]" subst markdown
+  where
+    subst match =      
+      let lst = [last match]
+          num = tail (init match)
+          url = "https://github.com/jflex-de/jflex/issues/"
+      in "[#" ++ num ++ "](" ++ url ++ num ++ ")" ++ lst
+
+
+substAll :: String -> (String -> String) -> String -> String
+substAll expr subst input =
+  let
+    (before, match, after) = input =~ expr
+  in
+    if not (null match)
+    then before ++ subst match ++ substAll expr subst after
+    else input
