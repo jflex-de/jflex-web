ca49d6f3cf98e131a07d8b6fce459d9b467b5625
Régis Décamps <decamps@users.sf.net>
2015-04-15 20:25:38 +0200

    Make the brand unclickable when it's invisible
    Because of the CSS animation, when hidden, the element is still there,
    with opacity 0.
    
    Change the href to none, so that it’s not clickable.

--- js/jflex.js
+++ js/jflex.js
@@ -4,14 +4,17 @@ var MATERIAL_HEADER_HEIGHT = 100;
 $(document).ready(function() {
     /* Material header gets sticky. */
 	$(window).scroll(function() {
-	    var navbar=$('#header');
-	    var content=$('#content');
+	    var navbar = $('#header');
+	    var content = $('#content');
+	    var brand = $('#brand');
 		if ($(this).scrollTop() > MATERIAL_HEADER_HEIGHT) {  
 			navbar.addClass('material-header-collapsed');
 			content.addClass('material-header-collapsed');
+			brand.attr('href', '#content');
 		} else {
 			navbar.removeClass('material-header-collapsed');
 			content.removeClass('material-header-collapsed');
+			brand.attr('href', null);
 		}
 	});
 });
